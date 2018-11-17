5675329bcfabd0b58cb3e78274dd25fe0fbf444a
Your Name <you@example.com>
2018-11-17 10:26:18 +0100

    Consegna Compito Tura

--- code/it/marconicivitavecchia/as2018_19/c5Ainf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Ainf/verifica/Main.java
@@ -12,7 +12,44 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(9,16);
+		StringBuilder sb=new StringBuilder();
+		sb.append("<html>");
+		
+		sb.append("<h1>SSH First Aid</h1>");
+		sb.append("<div style='display:table'>");
+		sb.append("<div style='display:table-row'>");
+		
+		sb.append("<div style='display:table-cell'>");
+		sb.append("<h1>Alias</h1>");
+		sb.append("</div>");
+		
+		sb.append("<div style='display:table-cell'>");
+		sb.append("<h1>Hostname</h1>");
+		sb.append("</div>");	
+		
+		sb.append("<div style='display:table-cell'>");
+		sb.append("<h1>Raspberry</h1>");
+		sb.append("</div>");
+		
+		sb.append("<div style='display:table-cell'>");
+		sb.append("<h1>server.marconi.it</h1>");
+		sb.append("</div>");
+		
+		sb.append("<div style='display:table-cell'>");
+		sb.append("<h1>CoreSwitch</h1>");
+		sb.append("</div>");
+		
+		sb.append("<div style='display:table-cell'>");
+		sb.append("<h1>10.0.100.1</h1>");
+		sb.append("</div>");
+		
+		sb.append("</div>");
+		sb.append("</div>");
+		sb.append("</html>");
+		
+		String lbl = sb.toString();
+		
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
 		
@@ -21,7 +58,7 @@ public class Main extends JFrame {
 		// TODO Convertire lo StringBuilder in String
 		
 		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		super.add(new JLabel(lbl));
 		super.setVisible(true);
 		 
 		System.out.println("Verifica Swing-GitHub");
